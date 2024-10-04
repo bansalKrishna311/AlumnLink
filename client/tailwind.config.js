0a24985dffc9347ca6a2ba1ec788b2c0047410f0
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-10-04 11:08:34 +0530

    Update tailwind.config.js to add custom colors and daisyui theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,13 +2,25 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src//*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5E35B1',  // Replacing primary with your color code
+        secondary: '#2196F3', // Replacing secondary with your color code
+        accent: '#FFD600',    // Replacing accent with your color code
+        lightBlue: '#90CAF9', // Adding light blue
+        lightPurple: '#B39DDB', // Adding light purple
+      },
+    },
   },
   plugins: [
     require('daisyui'),
   ],
-}
-
+  daisyui: {
+    themes: [
+      'light', 
+    ],
+  },
+};
