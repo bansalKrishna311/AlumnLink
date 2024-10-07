71bcc13c594d05ff6a225298e0505ef7c5d28d6f
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-10-07 10:44:24 +0530

    Add Navbar component to Landing side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Hero from './Sides/Landing/components/Hero'
+import Navbar from './Sides/Landing/components/Navbar'
 
 const App = () => {
-  return (
+  return (<>
+    <Navbar/>
     <Hero/>
+    </>
   )
 }
 
