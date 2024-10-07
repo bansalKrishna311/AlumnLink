e7636d03c4745130cce1b0b365d0fd9c1769c7a8
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-10-07 11:27:45 +0530

    navbar ready for now

--- client/src/Sides/Landing/components/Navbar.jsx
+++ client/src/Sides/Landing/components/Navbar.jsx
@@ -1,69 +1,7 @@
-// import React from 'react'
-// import logo from '../../../../src/assets/logo copy.png'
-// const Navbar = () => {
-//   return (
-//     <div className="navbar bg-base-100 ps-8 pe-8 border-b-2 border-gray-300">
-//     <div className="navbar-start">
-//       <div className="dropdown">
-//         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-//           <svg
-//             xmlns="http://www.w3.org/2000/svg"
-//             className="h-5 w-5"
-//             fill="none"
-//             viewBox="0 0 24 24"
-//             stroke="currentColor">
-//             <path
-//               strokeLinecap="round"
-//               strokeLinejoin="round"
-//               strokeWidth="2"
-//               d="M4 6h16M4 12h8m-8 6h16" />
-//           </svg>
-//         </div>
-//         <ul
-//           tabIndex={0}
-//           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-//           <li><a>Item 1</a></li>
-//           <li>
-//             <a>Parent</a>
-//             <ul className="p-2">
-//               <li><a>Submenu 1</a></li>
-//               <li><a>Submenu 2</a></li>
-//             </ul>
-//           </li>
-//           <li><a>Item 3</a></li>
-//         </ul>
-//       </div>
-//       <a><img className='w-52' src={logo} alt="" /></a>
-//     </div>
-//     {/* <div className="navbar-center hidden lg:flex">
-//       <ul className="menu menu-horizontal px-1">
-//         <li><a>Item 1</a></li>
-//         <li>
-//           <details>
-//             <summary>Parent</summary>
-//             <ul className="p-2">
-//               <li><a>Submenu 1</a></li>
-//               <li><a>Submenu 2</a></li>
-//             </ul>
-//           </details>
-//         </li>
-//         <li><a>Item 3</a></li>
-//       </ul>
-//     </div> */}
-//     <div className="navbar-end">
-//       <a className="btn">Get Started </a>
-//     </div>
-//   </div>
-//   )
-// }
-
-// export default Navbar
-
-
-
 import React, { useState, useEffect } from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi'; // Import icons from react-icons
-import logo from '../../../../src/assets/logo copy.png'
+import logo from '../../../../src/assets/logo copy.png'; // Light mode logo
+import whiteLogo from '../../../assets/logo-white.png'; // Dark mode logo
 
 const Navbar = () => {
   const [theme, setTheme] = useState('light');
@@ -77,7 +15,12 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar bg-base-100 ps-8 pe-8 border-b-2 border-gray-300">
+    <div 
+      className="navbar bg-base-100 ps-8 pe-8 border-b-[0.5px]" 
+      style={{
+        borderBottomColor: theme === 'light' ? 'rgba(128, 128, 128, 0.5)' : 'rgba(128, 128, 128, 0.3)', // Adjusted border opacity
+      }}
+    >
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -111,7 +54,10 @@ const Navbar = () => {
             <li><a>Item 3</a></li>
           </ul>
         </div>
-        <a><img className='w-52' src={logo} alt="" /></a>
+        {/* Conditionally render logo based on the theme */}
+        <a>
+          <img className="w-52" src={theme === 'light' ? logo : whiteLogo} alt="Logo" />
+        </a>
       </div>
 
       <div className="navbar-end">
@@ -146,7 +92,6 @@ const Navbar = () => {
                 ) : (
                   <FiMoon className="h-5 w-5 mr-2 text-gray-600" />
                 )}
-
                 {/* Right Text */}
                 <span>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</span>
               </button>
