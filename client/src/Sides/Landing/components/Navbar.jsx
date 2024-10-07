4b0ee65c04ab85f93144162430ca86cb76f0fbd3
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2024-10-07 11:09:56 +0530

    navbar updated

--- client/src/Sides/Landing/components/Navbar.jsx
+++ client/src/Sides/Landing/components/Navbar.jsx
@@ -1,60 +1,161 @@
-import React from 'react'
+// import React from 'react'
+// import logo from '../../../../src/assets/logo copy.png'
+// const Navbar = () => {
+//   return (
+//     <div className="navbar bg-base-100 ps-8 pe-8 border-b-2 border-gray-300">
+//     <div className="navbar-start">
+//       <div className="dropdown">
+//         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             className="h-5 w-5"
+//             fill="none"
+//             viewBox="0 0 24 24"
+//             stroke="currentColor">
+//             <path
+//               strokeLinecap="round"
+//               strokeLinejoin="round"
+//               strokeWidth="2"
+//               d="M4 6h16M4 12h8m-8 6h16" />
+//           </svg>
+//         </div>
+//         <ul
+//           tabIndex={0}
+//           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+//           <li><a>Item 1</a></li>
+//           <li>
+//             <a>Parent</a>
+//             <ul className="p-2">
+//               <li><a>Submenu 1</a></li>
+//               <li><a>Submenu 2</a></li>
+//             </ul>
+//           </li>
+//           <li><a>Item 3</a></li>
+//         </ul>
+//       </div>
+//       <a><img className='w-52' src={logo} alt="" /></a>
+//     </div>
+//     {/* <div className="navbar-center hidden lg:flex">
+//       <ul className="menu menu-horizontal px-1">
+//         <li><a>Item 1</a></li>
+//         <li>
+//           <details>
+//             <summary>Parent</summary>
+//             <ul className="p-2">
+//               <li><a>Submenu 1</a></li>
+//               <li><a>Submenu 2</a></li>
+//             </ul>
+//           </details>
+//         </li>
+//         <li><a>Item 3</a></li>
+//       </ul>
+//     </div> */}
+//     <div className="navbar-end">
+//       <a className="btn">Get Started </a>
+//     </div>
+//   </div>
+//   )
+// }
+
+// export default Navbar
+
+
+
+import React, { useState, useEffect } from 'react';
+import { FiSun, FiMoon } from 'react-icons/fi'; // Import icons from react-icons
+import logo from '../../../../src/assets/logo copy.png'
 
 const Navbar = () => {
-  return (
-    <div className="navbar bg-base-100">
-    <div className="navbar-start">
-      <div className="dropdown">
-        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h8m-8 6h16" />
-          </svg>
-        </div>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li><a>Item 1</a></li>
-          <li>
-            <a>Parent</a>
-            <ul className="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
-      </div>
-      <a className="btn btn-ghost text-xl">daisyUI</a>
-    </div>
-    {/* <div className="navbar-center hidden lg:flex">
-      <ul className="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul className="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </details>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div> */}
-    <div className="navbar-end">
-      <a className="btn">Get Started </a>
-    </div>
-  </div>
-  )
-}
+  const [theme, setTheme] = useState('light');
 
-export default Navbar
+  useEffect(() => {
+    document.querySelector('html').setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <div className="navbar bg-base-100 ps-8 pe-8 border-b-2 border-gray-300">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li><a>Item 1</a></li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+              </ul>
+            </li>
+            <li><a>Item 3</a></li>
+          </ul>
+        </div>
+        <a><img className='w-52' src={logo} alt="" /></a>
+      </div>
+
+      <div className="navbar-end">
+        <a className="btn">Get Started</a>
+      </div>
+      <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <button tabIndex={0} className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-5 w-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path>
+            </svg>
+          </button>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <button onClick={toggleTheme} className="btn flex items-center justify-between">
+                {/* Left Icon */}
+                {theme === 'light' ? (
+                  <FiSun className="h-5 w-5 mr-2 text-yellow-500" />
+                ) : (
+                  <FiMoon className="h-5 w-5 mr-2 text-gray-600" />
+                )}
+
+                {/* Right Text */}
+                <span>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
