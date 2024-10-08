ad9a110ea1423578f0c4aad96a286ef5050329ad
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-10-08 12:00:11 +0530

    parallax card prototype ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Hero from './Sides/Landing/components/Hero'
 import Navbar from './Sides/Landing/components/Navbar'
 import LogoSlider from './Sides/Landing/components/LogoSlider'
+import StackCards from './Sides/Landing/components/StackCards/StackCards'
 
 const App = () => {
   return (<>
     <Navbar/>
     <Hero/>
-   <LogoSlider/>
+   {/* <LogoSlider/> */}
+
+ <StackCards/>
 
     </>
   )
