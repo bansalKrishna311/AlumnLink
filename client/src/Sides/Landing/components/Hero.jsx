cf26f14c75e3045f8e277364b335f617e9855423
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-10-11 12:02:13 +0530

    hurrrr

--- client/src/Sides/Landing/components/Hero.jsx
+++ client/src/Sides/Landing/components/Hero.jsx
@@ -6,7 +6,7 @@ import ParticlesComponent from './ParticlesComponent';
 
 const Hero = () => {
   return (
-    <div className="hero md:pt-20" style={{ minHeight: '100vh', position: 'relative', overflow: 'hidden' }}>
+    <div className="hero md:pt-20" >
       <ParticlesComponent />
 
       <div className="hero-content flex-col lg:flex-row-reverse align-middle justify-center items-center gap-24">
