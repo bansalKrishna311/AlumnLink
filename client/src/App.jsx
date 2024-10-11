67a1991497d7e60318abe470eaa6e9d99571651b
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2024-10-11 12:41:33 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,11 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react'; // Import useEffect for simulating loading
 import Navbar from './Sides/Landing/components/Navbar';
 import Bottomfootgutter from './Sides/Landing/components/Bottomfootgutter';
 import LandHome from './Sides/Landing/Pages/Home/LandHome';
-import React, { useState, useEffect } from 'react'; // Import useEffect for simulating loading
-import Hero from './Sides/Landing/components/Hero';
-import Navbar from './Sides/Landing/components/Navbar';
 import PageTitle from './PageTitle';
-import LogoSlider from './Sides/Landing/components/LogoSlider';
-import StackCards from './Sides/Landing/components/StackCards/StackCards';
-import Action from './Sides/Landing/components/Action';
-import TargetCustomer from './Sides/Landing/components/TargetCustomer';
-import Bottomfootgutter from './Sides/Landing/components/Bottomfootgutter';
-import ContactUs from './Sides/Landing/components/ContactUs';
 import Loader from './Loader'; // Import Loader component
-import ParticlesComponent from './Sides/Landing/components/ParticlesComponent';
+
 
 
 const App = () => {
