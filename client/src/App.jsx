e98978a2eba1de3b2fc6ce246662bb0308bb60ed
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2024-10-11 12:36:50 +0530

    all components calls in Home done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import Hero from './Sides/Landing/components/Hero';
 import Navbar from './Sides/Landing/components/Navbar';
-import LogoSlider from './Sides/Landing/components/LogoSlider';
-import StackCards from './Sides/Landing/components/StackCards/StackCards';
-import CardSlider from './Sides/Landing/components/CardSlider';
-import Action from './Sides/Landing/components/Action';
-import TargetCustomer from './Sides/Landing/components/TargetCustomer';
 import Bottomfootgutter from './Sides/Landing/components/Bottomfootgutter';
-import ContactUs from './Sides/Landing/components/ContactUs';
-import ParticlesComponent from './Sides/Landing/components/ParticlesComponent';  
+import LandHome from './Sides/Landing/Pages/Home/LandHome';
 
 const App = () => {
   return (
     <>
-      <ParticlesComponent /> {/* Add the particles background effect */}
+      
       <Navbar />
-      <Hero />
-      <StackCards />
-      <LogoSlider />
-      <TargetCustomer />
-      <ContactUs />
+    <LandHome/>
       <Bottomfootgutter />
     </>
   );
