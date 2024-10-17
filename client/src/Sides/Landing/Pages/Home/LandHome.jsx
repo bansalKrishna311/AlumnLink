accac2cac5f86d3b4d20283e89c567bec6e583cb
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2024-10-17 12:36:05 +0530

    contact form

--- client/src/Sides/Landing/Pages/Home/LandHome.jsx
+++ client/src/Sides/Landing/Pages/Home/LandHome.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import Hero from '../../components/Hero';
 import LogoSlider from '../../components/LogoSlider';
 import StackCards from '../../components/StackCards/StackCards';
-import CardSlider from '../../components/CardSlider';
-import Action from '../../components/Action';
 import TargetCustomer from '../../components/TargetCustomer';
-import ContactUs from '../../components/ContactUs';
+import ContactUs from '../ContactPage/ContactUs';
 import ParticlesComponent from '../../components/ParticlesComponent';  
 
 const LandHome = () => {
@@ -16,7 +14,7 @@ const LandHome = () => {
     <StackCards />
       <LogoSlider />
       <TargetCustomer />
-      {/* <ContactUs /> */}
+      <ContactUs />
     </>
   )
 }
