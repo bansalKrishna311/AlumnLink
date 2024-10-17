d663169f9e2d571cb1ad88351b9ed238d79d522f
Laxmi Rajput <laxmirajput725@gmail.com>
2024-10-17 12:56:26 +0530

    Sticky Footer

--- client/src/Sides/Landing/components/footer/Content.jsx
+++ client/src/Sides/Landing/components/footer/Content.jsx
@@ -1,109 +1,58 @@
-import React from 'react';
+import React from "react";
+import { PRODUCTS, RESOURCES, COMPANY, SUPPORT, Icons } from "./Menus";
 
-export default function Content() {
-  return (
-    <section className="bg-blue-50 dark:bg-slate-800" id="contact">
-      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
-        <div className="mb-4">
-          <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
-            <p className="text-base font-semibold uppercase tracking-wide text-blue-600 dark:text-blue-200">
-              Contact
-            </p>
-            <h2 className="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
-              Get in Touch
-            </h2>
-            <p className="mx-auto mt-4 max-w-3xl text-xl text-gray-600 dark:text-slate-400">
-              In hac habitasse platea dictumst
-            </p>
-          </div>
-        </div>
-        <div className="flex items-stretch justify-center">
-          <div className="grid md:grid-cols-2">
-            <div className="h-full pr-6">
-              <p className="mt-3 mb-12 text-lg text-gray-600 dark:text-slate-400">
-                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis nec ipsum orci. Ut scelerisque sagittis ante, ac tincidunt sem venenatis ut.
-              </p>
-              <ul className="mb-6 md:mb-0">
-                <li className="flex">
-                  <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6">
-                      <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
-                      <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
-                    </svg>
-                  </div>
-                  <div className="ml-4 mb-4">
-                    <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Our Address</h3>
-                    <p className="text-gray-600 dark:text-slate-400">1230 Maecenas Street Donec Road</p>
-                    <p className="text-gray-600 dark:text-slate-400">New York, EEUU</p>
-                  </div>
-                </li>
-                {/* Repeat similar structure for other contact details */}
-              </ul>
-            </div>
-            <div className="card h-fit max-w-6xl p-5 md:p-12" id="form">
-              <h2 className="mb-4 text-2xl font-bold dark:text-white">Ready to Get Started?</h2>
-              <form id="contactForm">
-                <div className="mb-6">
-                  <div className="mx-0 mb-1 sm:mb-4">
-                    <label htmlFor="name" className="pb-1 text-xs uppercase tracking-wider">Name</label>
-                    <input type="text" id="name" autoComplete="given-name" placeholder="Your name" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="name" />
-                  </div>
-                  <div className="mx-0 mb-1 sm:mb-4">
-                    <label htmlFor="email" className="pb-1 text-xs uppercase tracking-wider">Email</label>
-                    <input type="email" id="email" autoComplete="email" placeholder="Your email address" className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="email" />
-                  </div>
-                  <div className="mx-0 mb-1 sm:mb-4">
-                    <label htmlFor="textarea" className="pb-1 text-xs uppercase tracking-wider">Message</label>
-                    <textarea id="textarea" name="textarea" cols="30" rows="5" placeholder="Write your message..." className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"></textarea>
-                  </div>
-                </div>
-                <div className="text-center">
-                  <button type="submit" className="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md hover:bg-blue-700 sm:mb-0">Send Message</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+const Content = () => {
+  const Item = ({ Links, title }) => (
+    <ul>
+      <h1 className="mb-1 font-semibold">{title}</h1>
+      {Links.map((link) => (
+        <li key={link.name}>
+          <a
+            className="text-gray-400 hover:text-teal-400 duration-300 text-sm cursor-pointer leading-6"
+            href={link.link}
+          >
+            {link.name}
+          </a>
+        </li>
+      ))}
+    </ul>
   );
-}
 
-
-const Section1 = () => {
-  return (
-    <div>
-      <Nav />
+  const ItemsContainer = () => (
+    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 px-5 py-16">
+      <Item Links={PRODUCTS} title="PRODUCTS" />
+      <Item Links={RESOURCES} title="RESOURCES" />
+      <Item Links={COMPANY} title="COMPANY" />
+      <Item Links={SUPPORT} title="SUPPORT" />
     </div>
   );
-}
 
-const Section2 = () => {
-  return (
-    <div className='flex justify-between items-end'>
-      <h1 className='text-4xl md:text-[10vw] leading-[0.8] mt-10 text-base-content'>AlumnLink</h1>
-      <p className='text-base-content'>©copyright</p>
+  const SocialIcons = () => (
+    <div className="flex justify-center space-x-4">
+      {Icons.map((icon) => (
+        <a
+          key={icon.name}
+          href={icon.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-400 hover:text-teal-400 duration-300"
+        >
+          <i className={`fab ${icon.name}`}></i>
+        </a>
+      ))}
     </div>
   );
-}
 
-const Nav = () => {
   return (
-    <div className='flex shrink-0 gap-20'>
-      <div className='flex flex-col gap-2'>
-        <h3 className='mb-2 uppercase text-base-content'>About</h3>
-        <p className='text-base-content'>Home</p>
-        <p className='text-base-content'>Projects</p>
-        <p className='text-base-content'>Our Mission</p>
-        <p className='text-base-content'>Contact Us</p>
-      </div>
-      <div className='flex flex-col gap-2'>
-        <h3 className='mb-2 uppercase text-base-content'>Education</h3>
-        <p className='text-base-content'>News</p>
-        <p className='text-base-content'>Learn</p>
-        <p className='text-base-content'>Certification</p>
-        <p className='text-base-content'>Publications</p>
+    <div className="bg-gray-900 text-white">
+      <ItemsContainer />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-gray-400 text-sm pb-8 px-5">
+        <span>© 2020 Appy. All rights reserved.</span>
+        <span>Terms · Privacy Policy</span>
+        <SocialIcons />
       </div>
     </div>
   );
-}
+};
+
+export default Content;
