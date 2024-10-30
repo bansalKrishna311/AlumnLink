a8300572d6f7bcdf87c003110b13b7adbe9659af
bansalKrishna311 <bansalKrishna311@gmail.com>
2024-10-30 21:44:46 +0530

    removed linkedin ke saaare chhap

--- user/backend/controllers/post.controller.js
+++ user/backend/controllers/post.controller.js
@@ -19,13 +19,8 @@ export const getFeedPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
 	try {
-		const { content, image, type } = req.body;
-
+		const { content, image } = req.body;
 		let newPost;
-
-		if (!["discussion", "job", "internship", "event"].includes(type)) {
-			return res.status(400).json({ message: "Invalid post type" });
-		}
 
 		if (image) {
 			const imgResult = await cloudinary.uploader.upload(image);
@@ -33,13 +28,11 @@ export const createPost = async (req, res) => {
 				author: req.user._id,
 				content,
 				image: imgResult.secure_url,
-				type,
 			});
 		} else {
 			newPost = new Post({
 				author: req.user._id,
 				content,
-				type,
 			});
 		}
 
@@ -51,7 +44,6 @@ export const createPost = async (req, res) => {
 		res.status(500).json({ message: "Server error" });
 	}
 };
-
 
 export const deletePost = async (req, res) => {
 	try {
