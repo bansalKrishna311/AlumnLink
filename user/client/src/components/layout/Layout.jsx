0b80464a1e8642739c793b99b219fd046f879508
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2024-10-30 18:28:34 +0530

    .

--- user/client/src/components/layout/Layout.jsx
+++ user/client/src/components/layout/Layout.jsx
@@ -1,11 +1,19 @@
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
-	return (
-		<div className='min-h-screen bg-base-100'>
-			<Navbar />
-			<main className='max-w-7xl mx-auto px-4 py-6'>{children}</main>
-		</div>
-	);
+  const location = useLocation();
+  
+  // Check if the current path is login or signup
+  const isAuthPage = location.pathname === "/login" || location.pathname === "/signup";
+
+  return (
+    <div className='min-h-screen bg-base-100'>
+      {/* Render Navbar only if the user is not on the login or signup page */}
+      {!isAuthPage && <Navbar />}
+      <main className='max-w-7xl mx-auto px-4 py-6'>{children}</main>
+    </div>
+  );
 };
+
 export default Layout;
