7c75653692800b2d688225513e96711a8b85d808
bansalKrishna311 <bansalKrishna311@gmail.com>
2024-10-31 14:56:57 +0530

    pr raise kar bhai

--- user/client/src/pages/NetworkPage.jsx
+++ user/client/src/pages/NetworkPage.jsx
@@ -165,6 +165,7 @@ const NetworkPage = () => {
 														handleRemoveLink(link._id);
 													}}
 												>
+													
 													Remove
 												</button>
 											</div>
