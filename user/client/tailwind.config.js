ec1ffec5a4cc4d3b5de17c82813d74aae95f7fa5
bansalKrishna311 <bansalKrishna311@gmail.com>
2024-11-07 23:23:31 +0530

    Add utility functions, update Tailwind config, and configure path aliases

--- user/client/tailwind.config.js
+++ user/client/tailwind.config.js
@@ -2,11 +2,19 @@ import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
-	},
-	plugins: [daisyui],
+    	extend: {
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {}
+    	}
+    },
+	plugins: [daisyui, require("tailwindcss-animate")],
 	daisyui: {
 		themes: [
 			{
