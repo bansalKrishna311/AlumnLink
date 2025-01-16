208eb93df5c2a0b2dc7d7d391c22d23e9c8eab31
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2025-01-16 16:22:31 +0530

    Refactor JoinNetworkCalling page layout for improved spacing and visual consistency

--- user/client/src/pages/JoinNetworkCalling.jsx
+++ user/client/src/pages/JoinNetworkCalling.jsx
@@ -7,24 +7,24 @@ const JoinNetworkCalling = () => {
   const queryClient = useQueryClient();
 
   return (
-    <div className="min-h-screen flex flex-col justify-start items-center pt-4">
+    <div className="min-h-screen flex flex-col justify-start items-center pt-0">
       <h1 className="text-4xl font-bold text-center text-gradient bg-clip-text text-transparent bg-gradient-to-r from-purple-800 via-purple-400 to-blue-600 mt-6 mb-4">
         Want to be a part of Alumlink?
       </h1>
 
-      <p className="text-lg text-gray-700 text-center mb-6">
+      <p className="text-lg text-gray-700 text-center mb-1">
         Join a growing network of alumni, professionals, and students. Expand
         your connections and opportunities through the Alumlink community.
       </p>
 
       <JNImageSlider />
 
-      <p className="text-3xl font-bold text-center text-gradient bg-clip-text text-transparent bg-gradient-to-r from-black via-gray-400 to-gray-700 mt-6 mb-4">
+      <p className="text-3xl font-bold text-center text-gradient bg-clip-text text-transparent bg-gradient-to-r from-black via-gray-400 to-gray-700 mt-4 mb-4">
         Join Now and be part of your Alma Mater
       </p>
 
       {/* Buttons Container */}
-      <div className="flex flex-row gap-4 justify-center items-center mt-6">
+      <div className="flex flex-row gap-4 justify-center items-center mt-4">
         <div className="relative">
           <JoinNetwork />
         </div>
