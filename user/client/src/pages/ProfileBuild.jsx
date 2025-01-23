9479ce8099aa7dbfcb2f468b8ee66263275e53ba
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2025-01-23 10:18:04 +0530

    Refactor ProfileBuild component: remove unused sections for cleaner code

--- user/client/src/pages/ProfileBuild.jsx
+++ user/client/src/pages/ProfileBuild.jsx
@@ -4,9 +4,6 @@ import { axiosInstance } from "@/lib/axios";
 
 import ProfileHeader from "@/components/ProfileHeader";
 import AboutSection from "@/components/AboutSection";
-import ExperienceSection from "@/components/ExperienceSection";
-import EducationSection from "@/components/EducationSection";
-import SkillsSection from "@/components/SkillsSection";
 import toast from "react-hot-toast";
 
 const ProfilePage = () => {
@@ -45,9 +42,6 @@ const ProfilePage = () => {
 		<div className='max-w-4xl mx-auto p-4'>
 			<ProfileHeader userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
 			<AboutSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
-			<ExperienceSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
-			<EducationSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
-			<SkillsSection userData={userData} isOwnProfile={isOwnProfile} onSave={handleSave} />
 		</div>
 	);
 };
