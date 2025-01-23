f376b5c7650f8e2531d8d4440f70b7b3feb72413
bansalKrishna311 <bansalKrishna311@gmail.com>
2025-01-23 10:46:00 +0530

    Enable credentials for axios instance in production and development environments

--- user/client/src/lib/axios.js
+++ user/client/src/lib/axios.js
@@ -4,5 +4,5 @@ export const axiosInstance = axios.create({
 	baseURL: import.meta.env.MODE === "development" 
 		? "http://localhost:4000/api/v1" // Use your local development URL here
 		: "https://alumnlink-1.onrender.com/api/v1", // Use your production URL here
-	withCredentials: false,
+	withCredentials: true,
 });
