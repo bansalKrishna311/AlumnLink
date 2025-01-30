dd9d0a4323dee021018434774f132d6d81f27b79
bansalKrishna311 <bansalKrishna311@gmail.com>
2025-01-30 10:39:08 +0530

    allowed cors for local 5173 only

--- user/backend/server.js
+++ user/backend/server.js
@@ -26,7 +26,14 @@ const __dirname = path.resolve();
 
 
 
-	app.use(cors())
+if (process.env.NODE_ENV !== "production") {
+	app.use(
+		cors({
+			origin: "http://localhost:5173",
+			credentials: true,
+		})
+	);
+}
 	
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser());
