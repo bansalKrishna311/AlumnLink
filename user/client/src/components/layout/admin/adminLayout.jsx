82c9b4d4e44fde742372f1dca3d8df1d9108aa67
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2025-03-24 11:51:19 +0530

    refactor adminLayout to improve structure with SidebarInset and enhance layout for Admin Dashboard

--- user/client/src/components/layout/admin/adminLayout.jsx
+++ user/client/src/components/layout/admin/adminLayout.jsx
@@ -1,14 +1,21 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger, SidebarInset } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 
 export default function adminLayout({ children }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
+    <SidebarProvider className="w-full h-full flex">
+  <div className="flex w-full h-full">
+    <AppSidebar />
+    <SidebarInset className="flex-1 w-full">
+      <header className="flex h-14 items-center border-b px-4">
+        <SidebarTrigger className="mr-2" />
+        <h1 className="text-lg font-medium">Admin Dashboard</h1>
+      </header>
+      <main className="p-4 w-full">{children}</main>
+    </SidebarInset>
+  </div>
+</SidebarProvider>
+
   )
 }
+
