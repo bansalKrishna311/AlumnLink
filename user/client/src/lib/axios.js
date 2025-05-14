1bfd1b2da1c07adcacb1bf33b321d1da5239703b
bansakrishna311 <bansalkrishna311@gmail.com>
2025-05-14 13:09:48 +0530

    fix: Update axios base URL for production environment

--- user/client/src/lib/axios.js
+++ user/client/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
 	baseURL: import.meta.env.MODE === "development" 
 		? "http://localhost:4000/api/v1" // Use your local development URL here
 		// : "https://alumnlink-1.onrender.com/api/v1", // Use your production URL here
-		: "https://alumn-link-server.vercel.app/api/v1", // Use your production URL here
+		: "http://139.59.66.21:4000/api/v1", // Use your production URL here
 	withCredentials: true,
 });
