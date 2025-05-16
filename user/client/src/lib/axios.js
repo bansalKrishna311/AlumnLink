3578c41c4e37160480a71fd1642b5055d8cbf87d
bansakrishna311 <bansalkrishna311@gmail.com>
2025-05-16 11:54:32 +0530

    fix: Update axios base URL for development environment to use localhost

--- user/client/src/lib/axios.js
+++ user/client/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
 	baseURL: import.meta.env.MODE === "development" 
-		? "http://139.59.66.21:4000/api/v1" 
+		? "http://localhost:4000/api/v1" 
 		: "http://139.59.66.21:4000/api/v1",
 	withCredentials: true,
 });
