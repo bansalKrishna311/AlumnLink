6a17584f905afae9b22fef7cb93a321a4d76138a
bansakrishna311 <bansalkrishna311@gmail.com>
2025-07-15 15:19:57 +0530

    fix: Update redirect URIs to use the new production server address

--- user/client/src/pages/auth/LoginPage.jsx
+++ user/client/src/pages/auth/LoginPage.jsx
@@ -45,7 +45,7 @@ const LoginPage = () => {
     const params = new URLSearchParams({
       response_type: "code",
       client_id: import.meta.env.VITE_LINKEDIN_CLIENT_ID,
-      redirect_uri: "http://139.59.66.21:4000/api/v1/auth/linkedinCallback",
+      redirect_uri: "https://api.alumnlink.com//api/v1/auth/linkedinCallback",
       scope: "openid email profile",
     });
     window.location.href = `https://www.linkedin.com/oauth/v2/authorization?${params}`;
