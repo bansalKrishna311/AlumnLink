7dd9a5faca7750f0e19775ac7ab2a96cc413aec9
bansakrishna311 <bansalkrishna311@gmail.com>
2025-07-15 15:04:07 +0530

    fix: Update axios base URL for production environment to use the correct API endpoint

--- user/client/src/lib/axios.js
+++ user/client/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
 	baseURL: import.meta.env.MODE === "development" 
 		? "http://localhost:4000/api/v1" 
-		: "http://139.59.66.21:4000/api/v1",
+		: "https://api.alumnlink.com/api/v1",
 	withCredentials: true,
 });
